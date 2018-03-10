9da7a68906551ac80416db2391a63bbc62efe001
Daniel Hayes <danihaye@gmail.com>
2018-03-09 23:17:00 -0500

    successfully integrated amazon s3 for staticfiles storage

--- piweatherviewer/settings.py
+++ piweatherviewer/settings.py
@@ -10,7 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/2.0/ref/settings/
 """
 
-import os
+import os, csv
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'storages',
 ]
 
 MIDDLEWARE = [
@@ -118,4 +119,27 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
 
-STATIC_URL = '/static/'
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'viewer/static'),
+]
+
+with open(os.path.join(BASE_DIR, 'piweatherviewer', 'access_key_id.txt'), 'r') as f:
+    AWS_ACCESS_KEY_ID = f.readline().strip()
+
+with open(os.path.join(BASE_DIR, 'piweatherviewer', 'secret_access_key.txt.'), 'r') as f:
+    AWS_SECRET_ACCESS_KEY = f.readline().strip()
+    
+
+AWS_STORAGE_BUCKET_NAME = 'piweatherstation'
+AWS_S3_CUSTOM_DOMAIN = '%s.s3.amazonaws.com' % AWS_STORAGE_BUCKET_NAME
+
+AWS_S3_OBJECT_PARAMETERS = {
+    'CacheControl': 'max-age=86400'
+}
+
+AWS_LOCATION = 'static'
+STATICFILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
+
+STATIC_URL = 'https://%s/%s/' % (AWS_S3_CUSTOM_DOMAIN, AWS_LOCATION)
+
+DEFAULT_FILE_STORAGE = 'piweatherviewer.storage_backends.MediaStorage'
