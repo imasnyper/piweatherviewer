1b443050137e9eda81abd19c4c2a375007aa4127
Daniel Hayes <danihaye@gmail.com>
2018-03-10 00:29:48 -0500

    tyring to configure media storage now on S3

--- piweatherviewer/settings.py
+++ piweatherviewer/settings.py
@@ -143,3 +143,5 @@ STATICFILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
 STATIC_URL = 'https://%s/%s/' % (AWS_S3_CUSTOM_DOMAIN, AWS_LOCATION)
 
 DEFAULT_FILE_STORAGE = 'piweatherviewer.storage_backends.MediaStorage'
+
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
--- viewer/models.py
+++ viewer/models.py
@@ -1,3 +1,7 @@
 from django.db import models
+from django.utils import timezone
 
 # Create your models here.
+class Photo(models.Model):
+	photo = models.ImageField()
+	uploaded_at = models.DateTimeField(default=timezone.now())
