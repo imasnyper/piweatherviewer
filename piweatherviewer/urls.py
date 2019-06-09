2092ca7421bb1adf17f5877d008ae9824c65184b
Daniel Hayes <danihaye@gmail.com>
2019-06-09 17:12:19 -0400

    removing autfixture from urls

--- piweatherviewer/urls.py
+++ piweatherviewer/urls.py
@@ -16,9 +16,6 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
-import autofixture
-
-autofixture.autodiscover()
 
 from viewer import views
 
